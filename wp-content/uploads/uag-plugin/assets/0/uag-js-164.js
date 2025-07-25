2d831c221e21eb4e4643ac9d13b40acb0790afed
onmoonspayu <fanokopey49@gmail.com>
2025-07-26 01:21:07 +0530

    Add files via upload

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-164.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-6284027c', {"layout":"number","heading":"Experience","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":15,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-47bdbd76', {"layout":"number","heading":"Happy Clients","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":250,"totalNumber":250,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-9cfbb71f', {"layout":"number","heading":"Employees","numberPrefix":"","numberSuffix":"","startNumber":0,"endNumber":100,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+ });
---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-165.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-66d0176e' );
+});
+ });
